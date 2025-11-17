45fe3b3f5acf7e48cca381694c3d2ee61c6c5e9f
Jadhav-Prajwal <prajwaljadhav174@gmail.com>
2025-11-17 09:16:22 +0530

    Create password_checker.py

---
+++ password_checker/password_checker.py
@@ -0,0 +1,6 @@
+# Simple Demo Of How Apps Verify Passwords Behind The Scenes.
+Password = input("Enter Password: ")
+if Password == "Admin456":
+    print("Access Granted ")
+else:
+    print("Access Denied ")
